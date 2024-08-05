cca6318f21b19b21b4dbbf0f19662ac1d9306d3d
ditorbejo <ditoooriq@gmail.com>
2024-08-05 21:06:20 +0700

    menghapus file tidak perlu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
-import CardMoviecComponent from './components/CardMovieComponent.vue'
 import HomePage from './pages/HomePage.vue'
 </script>
 
